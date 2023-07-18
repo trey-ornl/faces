1a78709ce475413577d19350500ceb873528bcbb
Trey White <twhite@pinoak-login1.head.cm.us.cray.com>
2023-07-18 14:50:49 -0500

    Initial version with 6 messages.

---
+++ pinoak-run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+module load craype-accel-amd-gfx90a
+module load rocm/5.4.3
+module -t list
+export LD_LIBRARY_PATH="${CRAY_LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}"
+set -x
+TASKS=$(( $1 * $2 * $3 ))
+NODES=$(( ( TASKS - 1 ) / 8 + 1 ))
+EXE=pinoak-faces
+ldd "${EXE}" | grep -e mpi -e gtl -e amd
+export OMP_NUM_THREADS=8
+export MPICH_OFI_NIC_POLICY=GPU
+export MPICH_GPU_SUPPORT_ENABLED=1
+export MPICH_ENV_DISPLAY=1
+echo "$1 $2 $3 15 14 13 12 10 10 100" | srun -K --exclusive -u -p bardpeak -t 1:00 -c ${OMP_NUM_THREADS} -N ${NODES} -n ${TASKS} ${EXE}
--- v-26-messages/Faces.h
+++ v-26-messages/Faces.h
@@ -6,10 +6,13 @@
 class Faces
 {
   public:
+    using Double2D = DArray<double,2>;
+    using Double3D = DArray<double,3>;
     using Double5D = DArray<double,5>;
     using Double6D = DArray<double,6>;
 
     Faces(int rank, int lx, int ly, int lz, int mx, int my, int mz, int n);
+    ~Faces();
     void share(Double6D &u, bool compute = true);
 
   protected:
@@ -19,8 +22,15 @@ class Faces
     int n_;
 
     int rank_,size_;
-    int iface_[6],nface_[3];
-    MPI_Request reqr_[6],reqs_[6];
+    int icorner_[8],iedge_[12],iface_[6],nedge_[3],nface_[3];
+    MPI_Request reqr_[26],reqs_[26];
+
+    static constexpr int nStreams_ = 2;
+    gpuStream_t stream_[nStreams_];
+
+    Double2D cornerr_, corners_;
+    Double3D xer_,yer_,zer_;
+    Double3D xes_,yes_,zes_;
 
     Double5D xfr_,yfr_,zfr_;
     Double5D xfs_,yfs_,zfs_;
